52b84ffd90ad7b4c1e6333552e7c0563d8f5cd0e
Benoît Vézina <benoit@vezina.biz>
2017-02-25 22:54:21 -0500

    fixed typo

--- models/stockQuant.py
+++ models/stockQuant.py
@@ -19,7 +19,7 @@ class stockQuant(models.Model):
         help='Weight of the container when empty. ')
     
     container_state = fields.Selection(
-        selectio=[  
+        selection=[  
             ('O', 'Good condition'),
             ('L', 'Slightly bumped'),
             ('B', 'Bumped'),
